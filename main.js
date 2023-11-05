68931fa5a9262f35f8a35e7b9b2bd49c703ca4d8
mac <mac@MacBook-Air--Avazbek.local>
2023-11-05 21:52:30 +0500

    secont commit add sort funtion

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var elForm = document.querySelector(".js-form");
 var elInput = document.querySelector(".js-input")
 var elList = document.querySelector(".list");
 var elSelect = document.querySelector(".js-select");
+var elOption = document.querySelector(".select__sort");
+var elWeight = document.querySelector(".select__count");
 
 function pokeFunc(info){ 
   elList.innerHTML = null;
@@ -35,6 +37,9 @@ function pokeFunc(info){
     
     
     pokeItem.append(pokeTitle,pokeNum,pokeImg,pokeWeight,pokeTime,pokeText);
+    elList.appendChild(pokeItem);
+    
+    
     fragment.appendChild(pokeItem)
     elList.appendChild(fragment);
   }
@@ -43,28 +48,80 @@ function pokeFunc(info){
 function selectArr() {
   var optionArr = []
   pokemons.forEach(itemes => {
-      itemes.weaknesses.forEach(function (cate){
-          if(!optionArr.includes(cate)){
-              optionArr.push(cate)
-          }
-      })
-      
+    itemes.weaknesses.forEach(function (cate){
+      if(!optionArr.includes(cate)){
+        optionArr.push(cate)
+      }
     })
-    console.log(optionArr);
-
+    
+  })
+  console.log(optionArr);
   
   optionArr.forEach(ganre => {
-      var elOption = document.createElement("option");
-      elOption.textContent = ganre;
-      elOption.value = ganre
-      elSelect.appendChild(elOption);
+    var elOption = document.createElement("option");
+    elOption.textContent = ganre;
+    elOption.value = ganre
+    elSelect.appendChild(elOption);
   })
 }
-
 selectArr()
 
-elForm.addEventListener("keyup", function (evt) {
+
+function SelSort(pokeArr,optVal) {
+  if (optVal == "A-Z") {
+    pokeArr.sort((a,b)=> {
+      if (a.name > b.name) {
+        return 1;
+      }else if (a.name < b.name) {
+        return -1;
+      }else {
+        return 0;
+      }
+    })
+  }
+  if (optVal == "Z-A") {
+    pokeArr.sort((a,b)=> {
+      if (a.name > b.name) {
+        return -1;
+      }else if (a.name < b.name) {
+        return 1;
+      }else {
+        return 0;
+      }
+      
+    })
+  }
+}
+
+function weightSort(pokeArr,weightVal) {
+  if (weightVal == "Max") {
+    pokeArr.sort((a,b)=> {
+      if (a.spawn_chance > b.spawn_chance) {
+        return 1;
+      }else if (a.spawn_chance < b.spawn_chance) {
+        return -1;
+      }else {
+        return 0;
+      }
+    })
+  }
+  if (weightVal == "Min") {
+    pokeArr.sort((a,b)=> {
+      if (a.spawn_chance > b.spawn_chance) {
+        return -1;
+      }else if (a.spawn_chance < b.spawn_chance) {
+        return 1;
+      }else {
+        return 0;
+      }
+      
+    })
+  }
+}
+elForm.addEventListener("submit", function (evt) {
   evt.preventDefault(); 
+  var weightVal = elWeight.value;
+  var optVal = elOption.value;
   var inputValue = elInput.value;
   var selectVal = elSelect.value;
   var newPoke = new RegExp(inputValue , "gi")
@@ -73,9 +130,11 @@ elForm.addEventListener("keyup", function (evt) {
   var pokeArr = pokemons.filter(item => {
     return item.name.match(newPoke)&& (item.weaknesses.includes(selectVal) || selectVal ==("all") );
   });
-
   console.log(pokeArr);
+  
   if(pokeArr.length > 0){ 
+    SelSort(pokeArr,optVal)
+    weightSort(pokeArr,weightVal)
     pokeFunc(pokeArr);
   }else {
     elList.textContent = "There is no such thing";
